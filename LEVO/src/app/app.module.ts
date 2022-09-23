d5a4896f5842f4ff12824c14f31c883805c2c543
DGGC <imoralem@hotmail.com>
2022-09-23 13:08:48 +0200

    Estructura frontend

---
+++ LEVO/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, CoreModule, FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ LEVO/src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShellComponent } from './shell/shell.component';
+import { HeaderComponent } from './shell/header/header.component';
+import { FooterComponent } from './shell/footer/footer.component';
+import { MainComponent } from './shell/main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    ShellComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule, AppRoutingModule, FormsModule
+  ],
+  exports:[ShellComponent]
+})
+export class CoreModule { }
