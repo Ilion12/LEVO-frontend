869f2ac8d770ee106700a0d4f2d4729d088e01e1
sdrfdim <sdrfdim@gmail.com>
2022-10-28 14:01:12 +0200

    Modificado formulario ALTA VEHICULO

--- frontend/src/app/vehiculo/vehiculo-routing.module.ts
+++ frontend/src/app/vehiculo/vehiculo-routing.module.ts
@@ -1,31 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MantenimientoFormComponent } from './vehiculos/mantenimiento-form/mantenimiento-form.component';
-import { VehiculoConsultaComponent } from './vehiculos/vehiculo-consulta/vehiculo-consulta.component';
-import { VehiculoEditarComponent } from './vehiculos/vehiculo-editar/vehiculo-editar.component';
-import { VehiculosComponent } from './vehiculos/vehiculos.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MantenimientoFormComponent } from "./vehiculos/mantenimiento-form/mantenimiento-form.component";
+import { VehiculoConsultaComponent } from "./vehiculos/vehiculo-consulta/vehiculo-consulta.component";
+import { VehiculoEditarComponent } from "./vehiculos/vehiculo-editar/vehiculo-editar.component";
+import { VehiculosComponent } from "./vehiculos/vehiculos.component";
 
 const routes: Routes = [
- {
-  path:'',
-  component:VehiculosComponent
- }, {
-    path:'mantenimientoForm/:id',
-    component:MantenimientoFormComponent
+  {
+    path: "",
+    component: VehiculosComponent,
   },
   {
-    path: 'consultar/:id',
-    component: VehiculoConsultaComponent
+    path: "mantenimientoForm/:id",
+    component: MantenimientoFormComponent,
   },
   {
-    path: 'editar/:id',
-    component: VehiculoEditarComponent
+    path: "consultar/:id",
+    component: VehiculoConsultaComponent,
   },
-  
+  {
+    path: "editar/:id",
+    component: VehiculoEditarComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class VehiculoRoutingModule { }
+export class VehiculoRoutingModule {}
